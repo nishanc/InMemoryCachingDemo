0fc3fbf8a59f4afb45a70a7fac7811896b532f41
Nishan Chathuranga Wickramarathna <inbox.nishan@gmail.com>
2022-03-26 16:38:10 +0530

    users controller

---
+++ InMemoryCaching.API/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace InMemoryCaching.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        
+    }
+}
